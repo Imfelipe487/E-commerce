76898922a756eea37330b84f1dccaa8f437e8592
LeonardoRG7 <leonardoriascos14@gmail.com>
2023-10-04 10:38:42 -0500

    update v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EcommerceComponent } from './modules/ecommerce/ecommerce.component';
+import { ErrorPageComponent } from './modules/components/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EcommerceComponent },
+  { path: '**', component: ErrorPageComponent, pathMatch: 'full' }, // cuando se digite una ruta incorrecta se redireccionara a la ruta raiz
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
